d0626855cdb4cdd517e5fd3d1db86a785f8fa550
roaraxali <roaraxyt@gmail.com>
2026-06-25 20:36:16 +0500

    fix: route docs subdomain directly to /documentation/user to avoid client-side routing clashes

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -1,13 +1,5 @@
-import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const headersList = await headers();
-  const host = headersList.get('host') || '';
-
-  if (host.includes('docs.mockrithm.me')) {
-    redirect('/user');
-  }
-
   redirect('/documentation/user');
 }
