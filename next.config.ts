b8b98bdfe08bf8084898f08a0153637ea3b92116
roaraxali <roaraxyt@gmail.com>
2026-06-29 00:22:21 +0500

    Ignore ESLint errors during Next build

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   serverExternalPackages: ["pdf-parse"],
   images: {
     remotePatterns: [
